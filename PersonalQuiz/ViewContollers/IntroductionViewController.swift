3c08e80f8c9c16b0b3878827ba18525e747dcf9e
Artur <petin.art@gmail.com>
2021-12-14 23:58:17 +0300

    continue...

---
+++ PersonalQuiz/ViewContollers/IntroductionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IntroductionViewController.swift
+//  PersonalQuiz
+//
+//  Created by Артур Петин on 05.12.2021.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwind(sague: UIStoryboardSegue) {}
+
+}
+
